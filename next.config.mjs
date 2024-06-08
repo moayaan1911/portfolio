d92af57a4ad3453c853f28b170466d0968c0c6d3
moayaan1911 <ayaangames@gmail.com>
2024-06-09 04:05:18 +0530

    next.js config fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+/**
+ * @format
+ * @type {import('next').NextConfig}
+ */
 
+const nextConfig = {
+  output: 'export',
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+};
 export default nextConfig;
